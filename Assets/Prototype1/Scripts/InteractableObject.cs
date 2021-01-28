abffcd61003845c27139ad8b41a46e91c166042f
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-01-28 21:20:13 +0700

    Create InteractableObject script

---
+++ Assets/Prototype1/Scripts/InteractableObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool interactable;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        interactable = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        interactable = false;
+    }
+}
