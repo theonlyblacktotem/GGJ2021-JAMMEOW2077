e349a984e8d7ed53c24a2d097edb5a671b8324ef
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-01-28 21:19:58 +0700

    Create Player Control Script

---
+++ Assets/Prototype1/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float walkSpeed;
+    [SerializeField] float climbSpeed;
+    public bool climbing;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        if (climbing)
+        {
+            transform.Translate(Vector2.up * y * climbSpeed * Time.deltaTime);
+        } else
+        {
+            transform.Translate(Vector2.right * x * walkSpeed * Time.deltaTime);
+        }
+    }
+}
