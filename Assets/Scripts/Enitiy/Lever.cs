ccb78501388a68a2986e62247ae14da180795b90
Sahatsarin Pawanna <sahatsarin07@gmail.com>
2021-01-31 03:30:47 +0700

    Lever can be pull and it lifts a platform

---
+++ Assets/Scripts/Enitiy/Lever.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public bool isPull;
+    public Collider2D whoIsPull;
+    private Collider2D[] whoIsStanding;
+    public GameObject platfrom;
+
+    private void Awake()
+    {
+        whoIsStanding = new Collider2D[2];
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Uncle"))
+        {
+            whoIsStanding[0] = collision;
+        } else if (collision.CompareTag("Child"))
+        {
+            whoIsStanding[1] = collision;
+        }
+    }    
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Uncle"))
+        {
+            if (collision == whoIsPull)
+            {
+                isPull = false;
+                whoIsPull = null;
+            }
+
+            whoIsStanding[0] = null;
+        } else if (collision.CompareTag("Child"))
+        {
+            if (collision == whoIsPull)
+            {
+                isPull = false;
+                whoIsPull = null;
+            }
+            whoIsStanding[1] = null;
+        }
+        
+    }
+
+
+    private void Update()
+    {
+        if (!isPull)
+        {
+            if (Input.GetKeyDown(KeyCode.Return)) {
+                if (whoIsStanding[0] != null) { 
+                    whoIsPull = whoIsStanding[0];
+                    isPull = true;
+                }
+
+            } else if (Input.GetKeyDown(KeyCode.Space)) {
+                if (whoIsStanding[1] != null)
+                {
+                    whoIsPull = whoIsStanding[1];
+                    isPull = true;
+                }
+            }
+        } else {
+            if ( (Input.GetKeyUp(KeyCode.Return) && whoIsPull.CompareTag("Uncle")) ||
+                    (Input.GetKeyUp(KeyCode.Space) && whoIsPull.CompareTag("Child")) )
+            {
+                isPull = false;
+                whoIsPull = null;
+            }
+        }
+        platfrom.GetComponent<Platform>().MoveUp(isPull);
+    }
+
+}
--- Assets/Scripts/Enitiy/Platform.cs
+++ Assets/Scripts/Enitiy/Platform.cs
@@ -5,6 +5,38 @@ using UnityEngine;
 public class Platform : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject platform;
+    public float maxHeight;
+    public float minHeight;
+    public float platformSpeed;
 
+    private Rigidbody2D obj_Rb;
+    private void Start()
+    {
+        obj_Rb = GetComponent<Rigidbody2D>();
+    }
+    public void MoveUp(bool isPull)
+    {
+        if (isPull)
+        {
+            if (transform.position.y <= maxHeight)
+            {
+                obj_Rb.velocity = Vector2.up * platformSpeed * Time.fixedDeltaTime;
+            }
+            else
+            {
+                obj_Rb.velocity = Vector2.zero;
+            }
+        }
+        else
+        {
+            if (transform.position.y >= minHeight)
+            {
+                obj_Rb.velocity = Vector2.up * -1* platformSpeed * Time.fixedDeltaTime;
+            }
+            else
+            {
+                obj_Rb.velocity = Vector2.zero;
+            }
+        }
+    }
 }
